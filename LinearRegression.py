486f97c36b252a38fb10949e0342af8efd164e4b
Abdallah Mamdouh <43778523+AbdallahMamdouh@users.noreply.github.com>
2020-05-01 16:17:58 +0200

    Add files via upload

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,54 +1,47 @@
 import numpy as np
 
-# !/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr 29 18:21:08 2020
 
-@author: abdallah mamdouh
-"""
+# feature scaling function
+def featureScale(x):
+    return (x - x.mean()) / x.std()
 
 
+# linear regression class
 class LinearRegression:
-    def featureNormalize(self,X):
-        mu=np.average(X)
-        sigma=np.std(X)
-        X=(X-mu)/sigma
-        return X
     def __init__(self):
-        self.m=0
-        self.n=0
-    def costFunction(self, X, y, Lambda=0):
+        self.theta = 0
+
+    # cost function that returns the cost J(theta) and the grad of J(theta)
+    def costFunction(self, X, y,Lambda):
         m, n = np.shape(X)
         y = np.reshape(y, (m, 1))
         hyp = X.dot(self.theta)
         diff = hyp - y
         error = np.sum(np.square(diff))
         reg = 0
-        tempTheta=0
+        tempTheta = 0
         if Lambda != 0:
-            reg = Lambda * np.square(np.sum(self.theta[1:, :]))
             tempTheta = self.theta
-            tempTheta[0]=0
-        J = (1 / 2 * m) * (error + reg)
-        grad = (1 / m) * (diff.T .dot(X)).reshape((n, 1))+(Lambda/m)*tempTheta
+            tempTheta[0] = 0
+            reg = Lambda * np.square(np.sum(tempTheta))
+        J = (1 / (2 * m)) * (error + reg)
+        grad = (1 / m) * (X.T.dot(diff)) + (Lambda / m) * tempTheta
         return J, grad
 
-    def train(self, X, y, alpha=0.0001, epochs=100000, Lambda=0):
-        X=self.featureNormalize(X)
-        self.m, self.n = np.shape(X)
-        X = np.append(np.ones((self.m, 1)), X, axis=1)
-        self.n+=1
-        self.theta = np.zeros((self.n, 1))
-        Jvec=[]
-        for e in range(epochs):
-            stage=epochs/100
+    def train(self, X, y, alpha=0.01, iters=10000, Lambda=0):
+        m, n = np.shape(X)
+        X = np.append(np.ones((m, 1)), X, axis=1)
+        n += 1
+        self.theta = np.zeros((n, 1))
+        Jvec = np.zeros(iters)
+        # using gradient descent algorithm to minimize J(theta)
+        for e in range(iters):
             J, grad = self.costFunction(X, y, Lambda)
             self.theta = self.theta - alpha * grad
-        return J,self.theta
+            Jvec[e] = J
+        return Jvec, self.theta
 
     def predict(self, x):
         m = np.shape(x)[0]
         x = np.append(np.ones((m, 1)), x, axis=1)
-        return  x.dot(self.theta)
-
+        return x.dot(self.theta).reshape(m)
--- main.py
+++ main.py
@@ -1,22 +1,39 @@
-from LinearRegression import LinearRegression
+from LinearRegression import LinearRegression, featureScale
+from sklearn.metrics import r2_score
 import pandas as pd
-import numpy as np
+import numpy as np, math
 import matplotlib.pyplot as plt
-data=pd.read_csv('Salary_Data.csv')
-X=data.iloc[:,:-1].values
-y=data.iloc[:,1].values
 from sklearn.model_selection import train_test_split
-X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.1,random_state=0)
 
-model= LinearRegression()
-J,theta=model.train(X_train,y_train)
-plt.scatter(x=X,y=y)
-x0 = np.linspace(0.75,10.5,100).reshape((100,1))
-y0 = model.predict(x0)
-plt.plot(x0, y0, '-r', label='y=X*theta')
-plt.title('Graph of y=X*theta')
-plt.xlabel('x', color='#1C2833')
-plt.ylabel('y', color='#1C2833')
-plt.legend(loc='upper left')
+# loading data
+data = pd.read_csv('Salary_Data.csv')
+data = featureScale(data)
+X = data.iloc[:, :-1].values
+y = data.iloc[:, -1].values
+
+# splitting data to test and train sets
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+
+# creating the model
+model = LinearRegression()
+
+# training the model
+MSE, theta = model.train(X_train, y_train, Lambda=0.01)
+
+# using the model to predict
+y_pred = model.predict(X_test)
+
+# calculating R2 score for my model
+R2 = r2_score(y_test, y_pred)
+print("R2 score = ", R2)
+
+# plotting cost function
 plt.grid()
+plt.plot(MSE)
 plt.show()
+
+# calculating mean square values and root mean square
+RMSE = math.sqrt(MSE[-1])
+print("mean square error = ", MSE[-1])
+print("root mean square error = ", RMSE)
